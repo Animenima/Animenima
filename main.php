824a0cf5c02493e345a83123826d800357a57b80
Animenima <animenima@gmail.com>
2022-04-07 20:02:50 -0400

    shit

--- main.php
+++ main.php
@@ -1,33 +1,28 @@
 <?php
+// Initialize the session
+session_start();
 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: index.php");
+    exit;
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-<title>Animenima</title>
 <head>
-	<style type='text/css'>
-		body {
-			background-color: gray;
-		}
-		div#one {
-			z-index: 1000;
-		}
-	</style>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
 </head>
 <body>
-	<h1 style="color:orange;
-	    text-align:center;
-			font-size:60px;">
-			Play Now
-	</h1>
-	<p style="color:red;
-	   text-align:center;
-		 font-size:30px;">
-		 Sign up
-	</p>
-
-	<a href="index.php">
-		Back to Title Page
-	</a>
+    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    </p>
 </body>
 </html>
